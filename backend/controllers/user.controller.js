5b5c04bfde3135aa7616e19c899edac6cf70b6d8
Prayas Raj <97390918+Prayas7632@users.noreply.github.com>
2024-12-23 12:55:46 +0530

    login logic implemented

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { User } from "../models/user.model";
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 export const register = async (req, res) => {
     try {
@@ -30,6 +31,48 @@ export const register = async (req, res) => {
         })
     } catch (error) {
         console.log(error);
+        
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const {email,password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({
+                message:"All fields are required",
+                success:false
+            })
+        };
+        const user = await User.findOne({email});
+        if(!user) {
+            return res.status(400).json({
+                message:"Incorrect email or password",
+                success:false
+            })
+        };
+        const isPasswordMatch = await bcrypt.compare(password,user.password);
+        if(!isPasswordMatch) {
+            return res.status(400).json({
+                message:"Incorrect email or password",
+                success:false
+            })
+        };
+
+        const tokenData = {
+            userId:user._id
+        }
+        const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {expiresIn:'1d'});
+        return res.status(200).cookie("token", token, {maxAge:1*24*60*60*1000, httpOnly:true, sameSite:'strict'}).json({
+            message:`Welcome Back ${user.fullname}`,
+            user:{
+                _id:user._id,
+                fullname:user.fullname,
+                email:user.email,
+            },
+            success:true
+        })
+    } catch (error) {
         
     }
 }
