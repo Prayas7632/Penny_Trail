00b4f51b618726e45c7295f9b955643da6d3f1bb
Prayas Raj <97390918+Prayas7632@users.noreply.github.com>
2025-01-26 12:49:41 +0530

    backend setup

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express, { urlencoded } from 'express'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config({});
+
+const app = express()
+const PORT = 8080
+
+//middleware
+app.use(express.json())
+app.use(urlencoded({extended:true}))
+app.use(cookieParser());
+const corsOptions = {
+     origin:"http://localhost:5173",
+     credentials: true
+}
+
+app.use(cors(corsOptions));
+
+
+app.listen(8080, ()=> {
+     console.log(`Server listen at port ${PORT}`)
+})
